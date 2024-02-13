6e5ff6f6342ef42da367122fb08739f96fb883b3
Artem <vebulogmetra@yandex.ru>
2024-02-13 22:40:34 +0300

    create base db models

---
+++ src/models/events.py
@@ -0,0 +1,27 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
+
+from src.schemas.events import EventSchemaOut
+from src.storages.database import Base
+
+
+class Events(Base):
+    __tablename__ = "events"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    expired_at: Mapped[datetime]
+    created_at: Mapped[datetime]
+    owner_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+    def to_schema(self) -> EventSchemaOut:
+        return EventSchemaOut(
+            id=self.id,
+            title=self.title,
+            description=self.description,
+            owner_id=self.owner_id,
+            expired_at=self.expired_at,
+            created_at=self.created_at,
+        )
