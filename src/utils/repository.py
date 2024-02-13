57914c8e777321782c35c830858109e2eda7f8de
Artem <vebulogmetra@yandex.ru>
2024-02-13 22:29:17 +0300

    add sqlalchemy repository

--- src/utils/repository.py
+++ src/utils/repository.py
@@ -12,3 +12,32 @@ class AbstractRepository(ABC):
     @abstractmethod
     async def find_all():
         raise NotImplementedError
+
+
+class SQLAlchemyRepository(AbstractRepository):
+    model = None
+
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def add_one(self, data: dict) -> int:
+        stmt = insert(self.model).values(**data).returning(self.model.id)
+        res = await self.session.execute(stmt)
+        return res.scalar_one()
+
+    async def edit_one(self, id: int, data: dict) -> int:
+        stmt = update(self.model).values(**data).filter_by(id=id).returning(self.model.id)
+        res = await self.session.execute(stmt)
+        return res.scalar_one()
+
+    async def find_all(self):
+        stmt = select(self.model)
+        res = await self.session.execute(stmt)
+        res = [row[0].to_schema() for row in res.all()]
+        return res
+
+    async def find_one(self, **filter_by):
+        stmt = select(self.model).filter_by(**filter_by)
+        res = await self.session.execute(stmt)
+        res = res.scalar_one().to_schema()
+        return res
