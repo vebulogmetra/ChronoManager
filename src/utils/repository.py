3814d8da68430e714b04b231cfaf0909209e275e
Artem <vebulogmetra@yandex.ru>
2024-02-13 22:28:44 +0300

    add absctract repository

---
+++ src/utils/repository.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+from sqlalchemy import insert, select, update
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+class AbstractRepository(ABC):
+    @abstractmethod
+    async def add_one():
+        raise NotImplementedError
+
+    @abstractmethod
+    async def find_all():
+        raise NotImplementedError
