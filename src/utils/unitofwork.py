b4d3c0488b860d3e11c9c0e144316a564cde53f3
Artem <vebulogmetra@yandex.ru>
2024-02-16 00:17:52 +0300

    implement UOW

---
+++ src/utils/unitofwork.py
@@ -0,0 +1,47 @@
+from abc import ABC, abstractmethod
+from typing import Type
+
+from src.storages.database import async_session_maker
+from src.repositories.events import EventsRepository
+from src.repositories.users import UsersRepository
+
+
+class IUnitOfWork(ABC):
+    users: Type[UsersRepository]
+    events: Type[EventsRepository]
+
+    @abstractmethod
+    def __init__(self): ...
+
+    @abstractmethod
+    async def __aenter__(self): ...
+
+    @abstractmethod
+    async def __aexit__(self, *args): ...
+
+    @abstractmethod
+    async def commit(self): ...
+
+    @abstractmethod
+    async def rollback(self): ...
+
+
+class UnitOfWork:
+    def __init__(self):
+        self.session_factory = async_session_maker
+
+    async def __aenter__(self):
+        self.session = self.session_factory()
+
+        self.users = UsersRepository(self.session)
+        self.events = EventsRepository(self.session)
+
+    async def __aexit__(self, *args):
+        await self.rollback()
+        await self.session.close()
+
+    async def commit(self):
+        await self.session.commit()
+
+    async def rollback(self):
+        await self.session.rollback()
