923853281ccce2f33a8a55a14469cb23fa0e4973
Artem <vebulogmetra@yandex.ru>
2024-02-17 10:13:10 +0300

    add optional field 'tg_user_id' to users model

--- src/models/users.py
+++ src/models/users.py
@@ -8,12 +8,14 @@ class Users(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str]
+    email: Mapped[str] = mapped_column(nullable=True)
+    tg_user_id: Mapped[int] = mapped_column(nullable=True)
     created_at: Mapped[datetime] = mapped_column(default=datetime.now)
 
     def to_schema(self) -> UserSchemaOut:
         return UserSchemaOut(
             id=self.id,
             email=self.email,
+            tg_user_id=self.tg_user_id,
             created_at=self.created_at,
         )
